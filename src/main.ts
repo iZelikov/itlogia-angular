4f628028258de65488be924e0fc87db79574b46b
iZelikov <ivan@zelikov.name>
2025-05-04 16:54:15 +0300

    day 4

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { CarsComponent } from './app/cars/cars.component';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
+  imports: [CarsComponent],
+  template: `<app-cars></app-cars>`,
 })
 export class App {
   name = 'Angular';
